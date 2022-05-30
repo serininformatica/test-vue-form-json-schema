bc71a5b4640913d0659bc57038043e3c0557defd
Serin Informatica <46899309+serininformatica@users.noreply.github.com>
2022-05-30 15:36:02 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <ExampleOne/>
+  </div>
+</template>
+
+<script>
+import ExampleOne from "./components/example-one";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default {
+  name: "App",
+  components: {
+    ExampleOne
+  }
+};
+</script>
